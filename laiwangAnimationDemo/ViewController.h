88b04ca7e9e5200cb1df8468679eed3bf5c84874
taitanxiami <taitanxiami@gmail.com>
2015-11-29 00:03:52 +0800

    Initial Commit

---
+++ laiwangAnimationDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  laiwangAnimationDemo
+//
+//  Created by taitanxiami on 15/11/29.
+//  Copyright © 2015年 taitanxiami. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ laiwangAnimationDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  laiwangAnimationDemo
+//
+//  Created by taitanxiami on 15/11/29.
+//  Copyright © 2015年 taitanxiami. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
